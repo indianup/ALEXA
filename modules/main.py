c8b3d852b160d0c2cb99adada01e6b0885dfe32b
AnkitShakya95 <ankitpatni95@gmail.com>
2025-02-02 07:37:04 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -290,7 +290,8 @@ async def txt_handler(bot: Client, m: Message):
             
 
             if 'psitoffers.store' in url:
-             vid_id =  url.split("vid=")[-1]("&")[0]
+             vid_id = url.split("vid=")[1].split("&")[0]
+             print(f"vid_id = {vid_id}")
              url =  f"https://madxapi-d0cbf6ac738c.herokuapp.com/{vid_id}/master.m3u8?token={raw_text4}"
 
             if "youtu" in url:
