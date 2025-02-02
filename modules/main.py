3daf2a9ea613ae7ef40fa930e639ed02e79429d0
AnkitShakya95 <ankitpatni95@gmail.com>
2025-02-03 03:53:30 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -282,7 +282,7 @@ async def txt_handler(bot: Client, m: Message):
             
             
             elif '/master.mpd' in url:
-             vid_id =  url.split("/")[-2]
+             vid_id =  url.split("hls")[-1]
              url =  f"https://pw-links-api.onrender.com/process?v=https://sec1.pw.live/{vid_id}/master.mpd&quality={raw_text2}"
 
             name1 = links[i][0].replace("\t", "").replace(":", "").replace("/", "").replace("+", "").replace("#", "").replace("|", "").replace("@", "").replace("*", "").replace(".", "").replace("https", "").replace("http", "").strip()
