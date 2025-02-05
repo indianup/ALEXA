05490b0a4f103ae607f71f61e896d59ba7b2b875
AnkitShakya95 <ankitpatni95@gmail.com>
2025-02-05 22:54:50 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -317,8 +317,8 @@ async def txt_handler(bot: Client, m: Message):
 
             try:  
                 
-                cc = f'**<pre><code>🎞️ VID_ID: {str(count).zfill(3)}.</code></pre>\n\n```📄 Title:</code></pre>{name1} {res} @Ankit_Shakya73.mkv```\n\n<pre><code>🔖 Batch Name:</code></pre> {b_name}</code></pre>\n\n<pre><code>📥 Extracted By :</code></pre> {CR}**\n\n<pre><code>━━━━━✦𝐀𝐍𝐊𝐈𝐓❤️✦━━━━━</code></pre>'
-                cc1 = f'**<pre><code>📁 PDF_ID: {str(count).zfill(3)}.</code></pre>\n\n```📄 Title: {name1} @Ankit_Shakya73.pdf```\n\n<pre><code>🔖 Batch Name</code></pre>: {b_name}</code></pre>\n\n<pre><code>📥 Extracted By :</code></pre>{CR}**\n\n<pre><code>━━━━━✦𝐀𝐍𝐊𝐈𝐓❤️✦━━━━━</code></pre>'
+                cc = f'**<pre><code>🎞️ VID_ID: {str(count).zfill(3)}.</code></pre>\n\n**```📄 Title:</code></pre>{name1} {res} @Ankit_Shakya73.mkv```**\n\n<pre><code>🔖 Batch Name:</code></pre> {b_name}</code></pre>\n\n<pre><code>📥 Extracted By :</code></pre> {CR}**\n\n<pre><code>━━━━━✦𝐀𝐍𝐊𝐈𝐓❤️✦━━━━━</code></pre>'
+                cc1 = f'**<pre><code>📁 PDF_ID: {str(count).zfill(3)}.</code></pre>\n\n**```📄 Title: {name1} @Ankit_Shakya73.pdf```**\n\n<pre><code>🔖 Batch Name</code></pre>: {b_name}</code></pre>\n\n<pre><code>📥 Extracted By :</code></pre>{CR}**\n\n<pre><code>━━━━━✦𝐀𝐍𝐊𝐈𝐓❤️✦━━━━━</code></pre>'
                     
                 
                 if "drive" in url:
