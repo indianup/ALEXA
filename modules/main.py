afdd10edea7b555a50687386de5ddafcbe723ab0
AnkitShakya95 <ankitpatni95@gmail.com>
2025-02-09 18:32:43 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -324,7 +324,7 @@ async def txt_handler(bot: Client, m: Message):
                print(f"PART3: {part3}")
                print(f"PART4: {part4}")
                print(f"PART5: {part5}")
-               url = f"kgs-v4.akamaized.net/kgs-cv/{part3}/{part4}/{part5}"
+               url = f"https://kgs-v4.akamaized.net/kgs-cv/{part3}/{part4}/{part5}"
             if '/onlineagriculture' in url:
                 # Splitting the URL
                parts = url.split("/")
